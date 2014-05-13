7d61e1e411a54339d4b5f5c474c27c82a6b70727
paninapress <NPacifico25@gmail.com>
2014-05-12 19:40:54 -0700

    challenges controller works for index/create

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,4 +1,25 @@
 class ChallengesController < ApplicationController
   def index
+    @challenges = Challenge.where(:user_id => current_user)
+  end
+  def create
+    # Create challenge
+    Challenge.create!(user_id: current_user.id, end_date: params[:challenge][:end_date])
+
+    # set the since_id with most recent search's max_id
+    h1_since_id = current_user.twitter.search("##{:hashtag1} since:#{Date.today}", {result_type: "recent", count: 1})
+      .attrs[:search_metadata][:max_id]
+    h2_since_id = current_user.twitter.search("##{:hashtag2} since:#{Date.today}", {result_type: "recent", count: 1})
+      .attrs[:search_metadata][:max_id]
+
+    # Create hashtags from params
+    challenge = Challenge.where(user_id: current_user.id).last
+    challenge.hashtags.create!(name: params[:challenge][:hashtag1], since_id: h1_since_id)
+    challenge.hashtags.create!(name: params[:challenge][:hashtag2], since_id: h2_since_id)
+    redirect_to "/challenges"
+  end
+
+  def show
+    @challenge = Challenge.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Hashtagbattle::Application.routes.draw do
   root "sites#index"
-  resource :challenges do
-    resource :hashtags
-  end
+
+  post '/challenges', to: 'challenges#create'
+  get '/challenges', to: 'challenges#index'
+
+
   get "/auth/twitter/callback", to: 'sessions#create'
   get '/signout', to: 'sessions#destroy', as: 'signout'
   get 'auth/failure', to: redirect('/')
